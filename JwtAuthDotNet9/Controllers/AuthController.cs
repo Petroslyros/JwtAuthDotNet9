0268f19dea2ca694c7f97d0187f37baf6d10e466
Petros <petrosluros@gmail.com>
2025-10-08 15:36:27 +0300

    Add project files.

---
+++ JwtAuthDotNet9/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using JwtAuthDotNet9.Entities;
+using JwtAuthDotNet9.Models;
+using JwtAuthDotNet9.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace JwtAuthDotNet9.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IAuthService authService) : ControllerBase
+    {
+
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            var user = await authService.RegisterAsync(request);
+            if (user == null)
+            {
+                return BadRequest("Username already exists");
+            }
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            var token = await authService.LoginAsync(request);
+            if (token == null)
+            {
+                return BadRequest("Invalid username or password.");
+            }
+
+            return Ok(token);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult AuthenticatedOnlyEndpoint()
+        {
+            return Ok("You are authenticated");
+        }
+
+
+    }
+}
